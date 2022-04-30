4cb68e429437857b40998778dcb2afa5980558f6
XYZTech <342994630@qq.com>
2022-04-30 17:03:24 +0800

    3.0.9
    3.0.9

---
+++ XYZTimeTest.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+import Foundation
+
+var greeting = "Hello, playground"
+
+
+let dateComponents = DateComponents(calendar: .current,timeZone: .current,year: 2022,month: 5,day: 1)
+dateComponents.timeZone
+
+dateComponents.year
+dateComponents.month
+dateComponents.day
+dateComponents.date
+= Date()
+//
+//DateComponents(calendar: <#T##Calendar?#>, timeZone: <#T##TimeZone?#>, era: <#T##Int?#>, year: <#T##Int?#>, month: <#T##Int?#>, day: <#T##Int?#>, hour: <#T##Int?#>, minute: <#T##Int?#>, second: <#T##Int?#>, nanosecond: <#T##Int?#>, weekday: <#T##Int?#>, weekdayOrdinal: <#T##Int?#>, quarter: <#T##Int?#>, weekOfMonth: <#T##Int?#>, weekOfYear: <#T##Int?#>, yearForWeekOfYear: <#T##Int?#>)
